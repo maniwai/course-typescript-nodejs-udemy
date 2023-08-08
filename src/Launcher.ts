297d77f393278324227a58ff43f8c4cba7e2b806
maniwai <maniwai@outlook.com>
2023-08-08 18:43:14 +0200

    initial commit

---
+++ src/Launcher.ts
@@ -0,0 +1,16 @@
+import { Server } from './Server';
+class Launcher {
+  // instance variables
+  private name: string;
+  private server: Server;
+
+  constructor() {
+    this.server = new Server();
+  }
+  public launchApp() {
+    console.log('started app');
+    this.server.createServer();
+  }
+}
+
+new Launcher().launchApp();
---
+++ src/Server.ts
@@ -0,0 +1,11 @@
+export class Server {
+  //   name: string;
+  //   server: Server;
+
+  //   constructor() {
+  //     this.server = new Server();
+  //   }
+  public createServer() {
+    console.log('created app');
+  }
+}
